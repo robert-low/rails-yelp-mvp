a6b551279607fa5cbba462eda6c8b2f719d8e453
Robert Low <robert.low91@gmail.com>
2023-03-30 17:27:12 +0100

    reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # get 'restaurants/new', to: 'restaurants#new'
   # post 'restaurants', to: 'restaurants#create'
   # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-  resources :restaurants, only: [:index, :show, :new, :create]
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new]
+  end
 end
