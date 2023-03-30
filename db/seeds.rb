c7e3ecde12a1a3891b166e232a4c58df9185e78a
Robert Low <robert.low91@gmail.com>
2023-03-30 15:44:37 +0100

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating new restaurants...'
+
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Faker::Restaurant.type
+  )
+  puts "Restaurant: #{restaurant.id} created!"
+end
+
+puts 'Finished!'
