12d82be87385ea9b971b7526360f2d5a4d016ccb
Robert Low <robert.low91@gmail.com>
2023-03-31 13:21:49 +0100

    edited validation for categories

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   CATEGORIES = %w[chinese italian japanese french belgian]
-  validates :name, :address, :category, presence: true
+  validates :name, :address, presence: true
   validates :category, inclusion: {
     in: CATEGORIES,
     message: '%<value> is not a valid size'
